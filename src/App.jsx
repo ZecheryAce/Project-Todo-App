7c8d892db82c3baae2f2f660cdb930d6f6469f5b
Zechery Acevedo <ZecheryAce@gmail.com>
2026-04-11 12:29:38 -0500

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Todos from "./pages/Todos";
+import Contact from "./pages/Contact";
+import Navbar from "./components/Navbar";
 
+function App() {
+    return (
+        <BrowserRouter>
+            <Navbar/>
+            <Routes>
+                <Route path="/todos" element={<Todos/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
+}
 
 export default App
