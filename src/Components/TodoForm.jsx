3f48d18b70487272f9452579ba2da1a01f98b5f5
Zechery Acevedo <ZecheryAce@gmail.com>
2026-04-11 13:00:22 -0500

    Todo Form

---
+++ src/Components/TodoForm.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+function TodoForm({ addTodo }) {
+    const [text, setText] = useState("");
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        addTodo(text);
+        setText("");
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                value={text}
+                onChange={e => setText(e.target.value)}
+                placeholder="Input Information"
+            />
+            <button type="submit">Add Task</button>
+        </form>
+    );
+}
+
+export default TodoForm;
