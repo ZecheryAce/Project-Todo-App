157615406c4de2fa2558402abcfb1f4d3ca896eb
Zechery Acevedo <ZecheryAce@gmail.com>
2026-04-12 13:17:59 -0500

    css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Todos from "./pages/Todos";
-import Contact from "./pages/Contact";
-import Navbar from "./components/Navbar";
+import Todos from "./Pages/Todos";
+import Contact from "./Pages/Contact";
+import Navbar from "./Components/Navbar";
 
 function App() {
     return (
